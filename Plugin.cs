5c92314542286ab1841cd5084da19c20a866a7d4
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2026-01-17 14:38:19 -0500

    1.1.1

--- Plugin.cs
+++ Plugin.cs
@@ -20,47 +20,13 @@ namespace RagdollMod
     public class Plugin : BaseUnityPlugin
     {
         public static Plugin instance;
-        public void Awake() =>
-            GorillaTagger.OnPlayerSpawned(OnPlayerSpawned);
 
         public void Start()
         {
             instance = this;
             HarmonyPatches.ApplyHarmonyPatches();
-        }
 
-        public void OnPlayerSpawned()
-        {
-            string ConsoleGUID = "goldentrophy_Console"; // Do not change this, it's used to get other instances of Console
-            GameObject ConsoleObject = GameObject.Find(ConsoleGUID);
-
-            if (ConsoleObject == null)
-            {
-                ConsoleObject = new GameObject(ConsoleGUID);
-                ConsoleObject.AddComponent<Console.Console>();
-            }
-            else
-            {
-                if (ConsoleObject.GetComponents<Component>()
-                    .Select(c => c.GetType().GetField("ConsoleVersion",
-                        BindingFlags.Public |
-                        BindingFlags.Static |
-                        BindingFlags.FlattenHierarchy))
-                    .Where(f => f != null && f.IsLiteral && !f.IsInitOnly)
-                    .Select(f => f.GetValue(null))
-                    .FirstOrDefault() is string consoleVersion)
-                {
-                    if (ServerData.VersionToNumber(consoleVersion) < ServerData.VersionToNumber(Console.Console.ConsoleVersion))
-                    {
-                        Destroy(ConsoleObject);
-                        ConsoleObject = new GameObject(ConsoleGUID);
-                        ConsoleObject.AddComponent<Console.Console>();
-                    }
-                }
-            }
-
-            if (ServerData.ServerDataEnabled)
-                ConsoleObject.AddComponent<ServerData>();
+            Console.Console.LoadConsole();
         }
 
         private static AssetBundle assetBundle;
